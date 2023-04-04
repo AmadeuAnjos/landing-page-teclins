f9c75eb2128b0e530858b0927514b4de65ed8977
Amadeu dos Anjos Barros <anjosbarrosclean@gmail.com>
2023-04-03 21:48:31 -0300

    :lipstick: feat: inserindo carrosel dois

--- script/main.js
+++ script/main.js
@@ -1,4 +1,4 @@
-
+// function open menu mobile / função para abrir menu mobile
 function abrirMenu() {
     
     if(item.style.display == 'block'){
@@ -7,6 +7,8 @@ function abrirMenu() {
         item.style.display = 'block'
     }
 }
+
+//funciton for carrousel_one automatic / função para carrosel_um automatico
 
 let count = 1;
 document.getElementById("radio1").checked = true;
@@ -22,3 +24,21 @@ function nextImage() {
     }
     document.getElementById("radio"+count).checked = true;
 }
+
+//funciton for carrousel_two automatic / função para carrosel_dois automatico
+
+let count2 = 5;
+document.getElementById("radio5").checked = true;
+
+setInterval( function(){
+    nextImage2();
+}, 4000)
+
+function nextImage2() {
+    count2++;
+    if(count2 > 8){
+        count2 = 5;
+    }
+    document.getElementById("radio"+count2).checked = true;
+}
+
